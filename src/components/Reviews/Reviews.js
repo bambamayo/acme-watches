fef12976321f2987a3725e7105be5d08e5c45b48
bambamayo <ayobamiagunroye@gmail.com>
2019-10-22 21:54:39 +0100

    adjust header height, pull reviews from firebase

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,89 +1,106 @@
-import React from "react";
+import React, { Component } from "react";
 import Img from "react-image";
-import { ProductConsumer } from "../../context";
 import Spinner2 from "../Spinner/Spinner2";
+import axios from "axios";
 
-const Reviews = () => {
-  return (
-    <section className="reviews">
-      <h2 className="section-header">What our customers are saying about us</h2>
-      <div className="reviews__images-container">
-        <ProductConsumer>
-          {consumer => {
-            let reviewImages = Object.keys(consumer.reviews).map(productKey => {
-              let details = consumer.reviews[productKey];
-              if (consumer.customerReview === null) {
-                return (
-                  <Img
-                    style={{
-                      opacity:
-                        consumer.defaultReview.customerImage ===
-                        details.customerImage
-                          ? "1"
-                          : "0.5"
-                    }}
-                    onClick={() =>
-                      consumer.showCustomerReviewHandler(productKey)
-                    }
-                    className="reviews__customer-image"
-                    src={details.customerImage}
-                    alt="customer thumbnail"
-                    key={productKey}
-                    loader={<Spinner2 />}
-                  />
-                );
-              } else {
-                return (
-                  <Img
-                    style={{
-                      opacity: consumer.customerReview === productKey ? 1 : 0.5
-                    }}
-                    onClick={() =>
-                      consumer.showCustomerReviewHandler(productKey)
-                    }
-                    className="reviews__customer-image"
-                    src={details.customerImage}
-                    alt="customer thumbnail"
-                    key={productKey}
-                    loader={<Spinner2 />}
-                  />
-                );
-              }
-            });
-            return reviewImages;
-          }}
-        </ProductConsumer>
-      </div>
-      <div className="review__message">
-        <ProductConsumer>
-          {consumer => {
-            if (consumer.customerReview === null) {
-              return (
-                <div
-                  className="review__message-container"
-                  style={{ zIndex: "3" }}
-                >
-                  <h3>{consumer.defaultReview.customerName}</h3>
-                  <p>{consumer.defaultReview.review}</p>
-                  <p>{`${consumer.defaultReview.location.city}, ${consumer.defaultReview.location.country}`}</p>
-                </div>
-              );
-            } else {
-              return (
-                <div className="review__message-container">
-                  <h3>
-                    {consumer.reviews[consumer.customerReview].customerName}
-                  </h3>
-                  <p>{consumer.reviews[consumer.customerReview].review}</p>
-                  <p>{`${consumer.reviews[consumer.customerReview].location.city}, ${consumer.reviews[consumer.customerReview].location.country}`}</p>
-                </div>
-              );
+class Reviews extends Component {
+  state = {
+    reviews: null,
+    loading: false,
+    error: null,
+    displayedReview: null
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    try {
+      const response = await axios.get(
+        "https://acme-project-930ec.firebaseio.com/reviews.json"
+      );
+      this.setState({
+        reviews: response.data,
+        loading: false,
+        displayedReview: response.data.review1
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        error: error.response.data.error,
+        loading: false
+      });
+    }
+  }
+
+  showCustomerReviewHandler = reviewId => {
+    if (this.state.reviews !== null) {
+      const reviews = { ...this.state.reviews };
+      const displayedReview = Object.keys(reviews).find(id => {
+        return id === reviewId;
+      });
+      console.log(this.state.displayedReview);
+      this.setState({
+        displayedReview: { ...reviews[displayedReview] }
+      });
+    }
+  };
+
+  render() {
+    let { displayedReview, reviews, loading } = this.state;
+    if (loading || reviews === null) {
+      return (
+        <section className="reviews">
+          <h2 className="section-header">
+            What our customers are saying about us
+          </h2>
+          <p
+            style={{
+              textAlign: "center",
+              fontSize: "2rem"
+            }}
+          >
+            Loading reviews.....
+          </p>
+        </section>
+      );
+    } else {
+      let reviewImages = Object.keys(reviews).map(productKey => {
+        let details = reviews[productKey];
+
+        return (
+          <Img
+            className="reviews__customer-image"
+            src={details.customerImage}
+            alt="customer thumbnail"
+            key={productKey}
+            onClick={() => this.showCustomerReviewHandler(productKey)}
+            style={
+              displayedReview.customerName === reviews[productKey].customerName
+                ? { opacity: "1" }
+                : { opacity: "0.5" }
             }
-          }}
-        </ProductConsumer>
-      </div>
-    </section>
-  );
-};
+            loader={<Spinner2 />}
+          />
+        );
+      });
+      return (
+        <section className="reviews">
+          <h2 className="section-header">
+            What our customers are saying about us
+          </h2>
+          <div className="reviews__images-container">{reviewImages}</div>
+          <div className="review__message">
+            <div className="review__message-container">
+              <h3>{displayedReview.customerName}</h3>
+              <p>{displayedReview.review}</p>
+              <p>{`${displayedReview.location.state}, ${displayedReview.location.country}`}</p>
+            </div>
+          </div>
+        </section>
+      );
+    }
+  }
+}
 
 export default Reviews;
